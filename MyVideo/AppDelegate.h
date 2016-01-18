405eb75714a5730e125dd572b3045ee307e7cd1d
田磊 <tianleios@163.com>
2016-01-18 16:28:46 +0800

    Initial Commit

---
+++ MyVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyVideo
+//
+//  Created by 田磊 on 16/1/18.
+//  Copyright © 2016年 田磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyVideo
+//
+//  Created by 田磊 on 16/1/18.
+//  Copyright © 2016年 田磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
